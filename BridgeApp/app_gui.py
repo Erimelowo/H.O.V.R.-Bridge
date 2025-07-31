60b7be44b4ac014771f10c40ce29a8c8ded08678
Erimel <marioluigivideo@gmail.com>
2025-07-31 13:54:54 -0400

    add setting to start minimized

--- BridgeApp/app_gui.py
+++ BridgeApp/app_gui.py
@@ -5,7 +5,7 @@ import time
 from app_config import AppConfig, PatternConfig
 from app_pattern import VibrationPattern
 
-WINDOW_NAME = "H.O.V.R. Bridge v0.1.1"
+WINDOW_NAME = "H.O.V.R. Bridge v0.2.0"
 
 LIST_SERVER_TYPE = ["OSC (VRChat)", "WebSocket (Resonite)"]
 
@@ -21,6 +21,7 @@ KEY_VIB_STR_OVERRIDE = '-VIB-STR-'
 KEY_BTN_TEST = '-BTN-TEST-'
 KEY_BTN_ADD_EXTERNAL = '-BTN-ADD-EXTERNAL-'
 KEY_BATTERY_THRESHOLD = '-BATTERY-'
+KEY_START_MINIMIZED = '-START-MINIMIZED-'
 
 # Pattern Config
 KEY_PROXIMITY = '-PROXY-'
@@ -64,8 +65,10 @@ class GUIRenderer:
                                       self.config.pattern_config_list[VibrationPattern.VELOCITY]))
 
         self.layout = [
-            [sg.Text('Bridge settings:', font='_ 14')],
-            [sg.Text("Server Type:"),
+            [sg.Text('Startup settings:', font='_ 13')],
+            [sg.Checkbox("Start minimized", default=self.config.start_minimized, key=KEY_START_MINIMIZED)],
+            [sg.Text('Bridge settings:', font='_ 13')],
+            [sg.Text("Server type:"),
              sg.InputCombo(LIST_SERVER_TYPE, LIST_SERVER_TYPE[self.config.server_type], key=KEY_SERVER_TYPE)],
             [sg.Text("Address:", size=9),
              sg.InputText(self.config.server_ip, k=KEY_REC_IP, size=16, tooltip="IP Address. Default is 127.0.0.1"),
@@ -74,10 +77,10 @@ class GUIRenderer:
              sg.Button("Apply", key=KEY_BTN_APPLY, tooltip="Apply and restart server.")],
             [sg.Text("Server status:"), self.osc_status_bar],
             [self.small_vertical_space()],
-            [sg.Text('Haptic settings:', font='_ 14')],
+            [sg.Text('Haptic settings:', font='_ 13')],
             [proximity_frame, velocity_frame],
             [self.small_vertical_space()],
-            [sg.Text('Trackers found:', font='_ 14')],
+            [sg.Text('Trackers found:', font='_ 13')],
             [self.tracker_frame],
             [sg.HSep()],
             [sg.Text("Made by Zelus (Z4urce), forked by Erimel (Erimelowo)", enable_events=True, font='Default 8 underline', key=KEY_OPEN_URL), sg.Sizegrip()]
@@ -227,6 +230,8 @@ class GUIRenderer:
         if self.window is None:
             self.window = sg.Window(WINDOW_NAME, self.layout, keep_on_top=False, finalize=True, alpha_channel=1.0, icon=b'iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABhGlDQ1BJQ0MgcHJvZmlsZQAAKJF9kT1Iw1AUhU9TpVIqDnYQEYxQneyiIo6likWwUNoKrTqYvPQPmjQkKS6OgmvBwZ/FqoOLs64OroIg+APiLjgpukiJ9yWFFjFeeLyP8+45vHcfIDSrTDV7YoCqWUY6ERdz+VUx8IoARuHDGIISM/VkZjELz/q6p06quyjP8u77s/qVgskAn0gcY7phEW8Qz25aOud94jArSwrxOfGkQRckfuS67PIb55LDAs8MG9n0PHGYWCx1sdzFrGyoxDPEEUXVKF/Iuaxw3uKsVuusfU/+wlBBW8lwndYIElhCEimIkFFHBVVYiNKukWIiTedxD/+w40+RSyZXBYwcC6hBheT4wf/g92zN4vSUmxSKA70vtv0xDgR2gVbDtr+Pbbt1AvifgSut4681gblP0hsdLXIEDGwDF9cdTd4DLneAoSddMiRH8tMSikXg/Yy+KQ8M3gLBNXdu7XOcPgBZmtXyDXBwCEyUKHvd49193XP7t6c9vx+NhXKx8+bP3gAAAAZiS0dEAAoACQAJxf4g9QAAAAlwSFlzAAAuIwAALiMBeKU/dgAAAAd0SU1FB+kHHwEhJYGc0KAAAAAZdEVYdENvbW1lbnQAQ3JlYXRlZCB3aXRoIEdJTVBXgQ4XAAAKhElEQVRYw9WXa3CV1bnHf+9l35O9d5KdkBtJyAWq4RJJEAQSEGI8CEpBqFiFcY7OUKWdWqmVg221FOdgUQcGsR0rxdp68CBEaCsUtEIsiIhAkpIEyH3nRpKd+76+1/MhymAPxzlnTr/0mVkz76z1vM/6r/96bgv+2cT/7jbpH2lP/r8ov7dhflaj/+MfyWk23WV1vDLSPtwOcMfsEmdGcfk7/SQVOp1xuf2Dw0xJktvPfvjH9/LSXS8ce/9o4P8N4MzTy0vjc8zVLx6seUKLqsNjil4HvL75ueeSTn50eqW/tuZewWInYphIgkBTs5ntsMc/edWRVg5M+5/sin8/0XFub+mX38kbJidZZ3sWexYlzyjdd+TFJVebtrwdCtRbslwn0p6eehygpGjGvylK7NubnloPhoaAgWDomLqKTdAZGwxMvef1vcX/awAOj2tS4MqB6QBjwVCO2hOqDCqxJbrFzO7YWR+gV+nQr4UnKYeu7Tp09PDPpk+bvmznju0LJ/gSWLOgkNXZQ9w3aZQZPoM4txvlagstnYGVAIGqDYuPnt8t3LifcDNUrn/xFUUuj87y+OIPhyPR75dkzNlYOKessb2r/oeLpxV/Z07JnMzp06bnhIIhX09/P167jGR3MTAwyB9f3cycKQrFMxNp61N46e1OzsTPbOnYvyfv3NuPpDgaerKnbj127msBCFny8F1e3+iypYUJ25uaFnbXXdv+yIpnRh5Ytjw9x+eeJdvsghIO0VL/GaniKaZkhegKTkVNX01/dzebN/+EB0oV1j+QQziksP+UlTf+1JH7+V9PtV4++GPnN+7fGv5yr6+EVN5rs+35G4qM8jZj7ty81KK2vtHqD8+2ZsQvSt50ZseffqVLRnV3xJjZHhju9ag9aVMsBzldH8OXu4Ykq8bl2k+p69X54P2PuHAFWjuGKLs9gWnZOkWT5cTlv/jZobyYXvHyb4813pQBW6HrKT2irnr8jpzv/qq5b7k5FpvkLvLtHHy74/yNei9sfda6rtTfIirRjMSJk4gmb2TwzC5CVjdvfq5zeM8+VE1BUaLsf6mAoileFFVgUPG+MnnB6xtv6oSuBYnzBUnSTY+ld/c7TZU2SaxOuTv7+2dX3us5+aNVC278qSyn9pEJXjJSsqbQMLio4dh7B4nzuPlrVRW7f76Njm4/A4MBguEwSx6vpubqCAI6CZaRp/78wcsVN3XClMdyPYHj7dWGX8+xlyU8qTmFE5UZRX0li1NeT//2O/cdKl2brKIs1LTYZ+FlfZQufGhLiz/U2d1QHTnXE/dzIz6RbPEso8EU0mevYHSgn6hucuB3b1I2DZ5em4s2GkHKm90wedZPb73pFcjfzco0qvp3iWHT0Jqj91/d84zFam1clLO28hjAu8Urlxiy8OMHzh6cB1C5peKNtAm3PDqSOg9GL1FR0srx0wF+vW2I2ZPycZaV0C+bXPi8htzUOB4rbsM5vYLXOicuffmbK458JROWlc93i5eTZqXOzGmXExOS9cF62+RHX4wBx77UWX2+8ihwFODBdU9stKdMfjRn0SLifRnEak4BAs2DaTRYAszA5C+HDlN0z13ExScwGLPw1kci99sbmbJ4Vd71QxfcUuC6rbj8eVl2fGcsOBY3NBAgze7CWVie7s38YOlw51D4Rpbmli5wpU+89fcVkeg3ZxYVY0RV2n76AjkPB6jrKeD57XtJSPDh0HW6YmG8zc0EQyqmCaqi8Fn1CG3uc4XXw/D2O+6crmrim/39vVbT1EnPzEFVY1hies6kqXNLZ61dd/DikUMKwKoH15VrmvXPmOIcayiE2NqM/+QpQkPdFCzJZ8O2eq71DWG1WJjnTePMYA+ybCXOk4gkWQiZMmOWTJpPnC4xvEnNw11ttcL3Nj3pOnOyJnjb7aXYnXH09nTT29VBcsoEHPFOxsKRJkGPro+MBP81ElUeAoHg6AiDfZ0sKswkIdXHxm+ZjJlp7PjNZebflkp7tx96krngTCYSijA6PISha4iSTCwSpsnfSsLdyy40/eaVYnnXth2hRF/SJympWXPDoTBpE9NJ8CXQUPc50XCMjMxJ+RaL9S+xWIRIeIzeni5GRod48uFM5hVI2N0O+roDBCIKrom38rdPq8hOsvKZQyIWi+H1JTE82Ic3MZkufwudXW2AjKJrM69HQXpm1rOB/sDWiqWrqDl/hmhMGV8QBOw2B1lZeUiyBedwI9/wGWzeMg2v04IkmQgidPcrPLUzhCfOw6WwD9l/kaTsAkRZRpIs7P/9a5TMupOO9hZ0TAxEHMtWszYt5pAAbi2+pb2rtfMH7a3NGKaIaRpggoiArmsM9F9jmlfk3skyiS4LHreMVZCQbQJDAQWbIeK1BPEQYEm+hT1HLtHhb8ff2kxrcyPh4AiYJoIkoygqhgnBmMb0lIT/EAHOnvjUn+jznYxFx9B1DUM30HQNVdeIRMKEomFGB3vo7g8SiWg0N44xFlCJDOtYJJGOa9l4HVnUt0aYEDeCAJiGjq4b6IYBQHd3O+54L6ZpYhomxsAAqqaOXc8D6WkTdgf6ehfGIiFsDheY4/O6oQFwLqAhOB3M0kOE6xRys93oHSDbBS4PuGjwRzhxxcYfzvtRdR3DNDG/NPKFaLqG1WojHI0ijI1QtqK063otqKmuPuDz+T5SlBCGrmOaJoZhAAKmYRIJh/CrFoaGfESDGlWnutEMAyWmc/pCLXvfPYEtrOJ2J6IbOqZpYBrGOPXCeMLt9F8heUIaGCZeO60P3rXOFAHqfni/EyA+zXOf0xX3cSwyhmEYGIaBME4opgnxzniisocr3dDSFuE/T3RTVRuk0/CxwOrF5/ZgtdrGaf5iGIaBKI5X/VgsiqaqGJhIFqH6ejVUUu13A7TUNoZSs1KXOJz2j3VNHTcAGIaJ1WZD1wxq9CghLZ69tcPs+7CLrZW9LMnoIa08nn4JNE3FNMfBm6aJKAhfgBARRYlIJITd4UIwhePXAfh8SlHdqyseBmipbwzftXRxhdtj3+Oy2zFME0QBm9XG/IpSnn3pBcpW38Om9d9iVaGbo/+eS/lMK/vebycai6FqGlaLFQDTMLFaLOMpV7IiCDI9nS24E5P7vR7r764DmPjIu8/ZTKPu+TV3OgAq91XG+q/1PuaKs74S57AjieO+arHbOPDWb0nMn8zKZ7aw6Cc7yMuLJyyomAKEYxEi0Qg2m338Ck0DWZIQBAlNi6HrMTQ1iiAYOy7V1IW+Ug3zv3f44t/3hu4E7xsOu7A+qkRd80rLiJ49TrbaS+eBTxi5xU6uWYuhG4RNFVVTMb+o7oIoYZgmVotMOBzEMFQA4hPTyF/z0ODIlfO/pv5rmtIbZU7p/AKL5Nn+g8fXLHdKApLVycDgKLOzazHdBTDYTI+ayKcNFq40tnH+YgOybCEaCeFyOhkIBBBFCYvVhmR3VDkTE544fWR//dd2xTeTsoV3pLy6a+cCny+5zGa1F0VGAxOvNF3KdkkSKRNzyMmfwZtvHeDDqouEQyHUSHBkdHT4cnBs5G82q1gj2mJ/+OTkGf9/68D/EQ/MZ5/fZL9w4ZJLNGU5Jy9tePeOX8b+aV7b/wUlU+0paexpAwAAAABJRU5ErkJggg==')
             self.window.set_resizable(False, True)
+            if (self.config.start_minimized):
+                self.window.minimize()
 
         # Update Layout if it's changed.
         if self.layout_dirty:
@@ -264,6 +269,9 @@ class GUIRenderer:
 
         for tracker in self.trackers:
             self.update_tracker_config(values, tracker)
+
+        # Update startup settings
+        self.config.start_minimized = values[KEY_START_MINIMIZED]
 
         # Update OSC Addresses
         self.config.server_type = LIST_SERVER_TYPE.index(values[KEY_SERVER_TYPE])
